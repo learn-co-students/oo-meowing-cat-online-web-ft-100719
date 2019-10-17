dbe42e846af4533005a5d067decaa5e838c01576
github username <github email address>
2019-10-16 18:29:18 -0700

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,6 +1,6 @@
 ## code your solution here. 
 class Cat
-    attr_accessor :name, :meow
+    attr_accessor :name, :meow   # attr_accessor takes the place of having to define the reader and writer methods
 
     def meow
         puts "meow!"
